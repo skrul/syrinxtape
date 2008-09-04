aef4db3525e4b0d891b91260ff0c2e3a58458e61
skrulx <skrulx@f51885ff-c955-0410-a2a8-f54c0c7d8d7e>
2008-09-04 03:41:24 +0000

    The Great Renaming part two comes to a close

--- trunk/components/src/stNetUtils.cpp
+++ trunk/components/src/stNetUtils.cpp
@@ -2,7 +2,7 @@
  * Copyright (C) 2008 by Steve Krulewitz <skrulx@gmail.com>
  * Licensed under GPLv2 or later, see file LICENSE in the xpi for details.
  */
-#include "sbUdpMulticastClient.h"
+#include "stNetUtils.h"
 
 #include <prnetdb.h>
 #include <nsAutoPtr.h>
